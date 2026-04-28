9c203f2f69427bb389fc4349cf45f57f139f13a3
Álvaro Rezende Martimiano <alvarovalim40@gmail.com>
2026-04-28 20:04:37 -0300

    Aula 01 e Aula 02 concluidas

---
+++ aula02/aula02.py
@@ -0,0 +1,17 @@
+casa = "Bonita" ; jorge = "Calvo" ; print(casa, jorge)
+
+# Exemplo de comentario de multiplas linhas
+
+"""
+num1 = 10
+num2 = 5
+num3 = 12
+"""
+
+if 50 > 30:
+    print("Maior")
+    print("Nice")
+print("End")    
+
+# Para o comando estar dentro do meu if
+# Ele vai precisar de um espaço ou recuo para que esteja dentro do if
